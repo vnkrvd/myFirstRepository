d264a9991c285ce0e6960f9387d6b12571a1b1b4
Dunya <darya.vinokurova.2002@gmail.com>
2021-06-03 20:25:39 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
+void input(int*, int*);
 int main()
 {
+int num1, num2;
+input(&num1,&num2);
+cout <<"Numbers: "<<num1 <<"; "<<num2<< end;
 return 0;
+}
+void input(int*, int*);
+{
+cout << "Enter first number";
+cin>>*a;
+cout << "Enter second number";
+cin >>*b;
 }
